035947127476cf928c3da6def0de5148710d740a
QianYang <qianyangjy@163.com>
2019-04-02 15:16:14 +0800

    测试脚本更新

--- script/script.lua
+++ script/script.lua
@@ -1,11 +1,11 @@
 local tasklib = require("tasklib")
 
 gui.settitle("测试脚本")
-gui.addmsgtype("类型1")
-gui.addmsgtype("类型2")
-gui.addmsgtype("类型3")
-gui.addmsgtype("类型4")
-gui.addmsgtype("类型5")
+local mt1 = gui.addmsgtype("类型1")
+local mt2 = gui.addmsgtype("类型2")
+local mt3 = gui.addmsgtype("类型3")
+local mt4 = gui.addmsgtype("类型4")
+local mt5 = gui.addmsgtype("类型5")
 
 task = tasklib:new()
 
@@ -17,15 +17,51 @@ end
 gui.setfntitle(1,"查看KV表")
 task:regist_fn_function(1, Fn1)
 
+function Fn2()
+    gui.toclipboard("hehe")
+end
+gui.setfntitle(2,"剪贴板")
+task:regist_fn_function(2, Fn2)
+
+function Fn3()
+    gui.setkv("颜色1", "255,0,0", "255,0,0")
+    gui.setkv("颜色2", "228,88,148", "228,88,148")
+    gui.setkv("颜色3", "0,255,0", "0,255,0")
+    gui.setkv("颜色4", ".....")
+end
+gui.setfntitle(3,"颜色")
+task:regist_fn_function(3, Fn3)
+
+function Fn4()
+    gui.playmedia("E:\\tools\\LuaLabBinV23\\music\\冯提莫 - 佛系少女.mp3")
+end
+gui.setfntitle(4,"mp3")
+task:regist_fn_function(4, Fn4)
+
+function Fn5()
+    gui.stopmedia()
+end
+gui.setfntitle(5,"stop mp3")
+task:regist_fn_function(5, Fn5)
+
 function taskA()
-    local tag = 0
     while true do
-        tag = tag + 1
-        gui.push("type1 -- " .. gui.getmstime())
-        gui.setkv("count", tag)
-        task:spawn()
+        gui.push(mt1, "type1 -- " .. gui.getmstime())
+        gui.push(mt2, "type2 -- " .. gui.getmstime())
+        gui.push(mt3, "type3 -- " .. gui.getmstime())
+        gui.push(mt4, "type4 -- " .. gui.getmstime())
+        gui.push(mt5, "type5 -- " .. gui.getmstime())
+        task:sleep(5)
+    end
+end
+
+function taskB()
+    while true do
+        gui.push("timestamp:" .. gui.getmstime())
+        task:sleep(1)
     end
 end
 
 task:add_task(taskA)
+--task:add_task(taskB)
 task:run_loop()
